ed40443f8d8311b0a7500a3e0a4b46db093e65b9
Artem Lienko <tema.nesmeyanow@yandex.ru>
2025-11-16 13:21:03 +0300

    refactor: изменены префабы головоломок
    Canvas помещены в пустые GameObject

--- Game/Assets/Scripts/Gameplay/Safe.cs
+++ Game/Assets/Scripts/Gameplay/Safe.cs
@@ -3,7 +3,22 @@ using UnityEngine;
 public class Safe : MonoBehaviour
 {
     [SerializeField] private GameObject OpenText;
-    
+    private bool _mayOpen = false;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OpenText.SetActive(true);
+        _mayOpen = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        OpenText?.SetActive(false);
+        _mayOpen = false;
+    }
+
+
 
 
 }
