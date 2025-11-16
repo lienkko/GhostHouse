b57cd338abe0856932f9cf4f10aa3ff70bc9266d
Artem Lienko <tema.nesmeyanow@yandex.ru>
2025-11-16 15:27:58 +0300

    feat: добавлена логика для сейфа
    Добавлено открытие головоломки при попытке открыть сейф
    Добавлено открытие сейфа после решения головоломки
    Добавлен преждевременный выход из головоломки (Escape)
    Добавлен префаб сейфа

--- Game/Assets/Scripts/Gameplay/Safe.cs
+++ Game/Assets/Scripts/Gameplay/Safe.cs
@@ -1,24 +1,83 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Safe : MonoBehaviour
 {
-    [SerializeField] private GameObject OpenText;
+    private string[] _puzzleNames = new string[] { "Circles", "Star" };
+    [SerializeField] private GameObject _openText;
     private bool _mayOpen = false;
+    private bool _isInPuzzle = false;
+    private GameObject _puzzle;
+    private Button _puzzleButton;
+    private PlayerController _playerController;
 
+
+    private void Update()
+    {
+        if (_mayOpen && Input.GetKeyDown(KeyCode.E))
+        {
+            OpenPuzzle();
+            return;
+        }
+        if (_isInPuzzle && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ClosePuzzle();
+            return;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        OpenText.SetActive(true);
-        _mayOpen = true;
+        ShowOpenText(true);
+        _playerController = collision.GetComponent<PlayerController>();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        OpenText?.SetActive(false);
-        _mayOpen = false;
+        ShowOpenText(false);
+        _playerController = null;
     }
 
+    private void ShowOpenText(bool state)
+    {
+        _openText.SetActive(state);
+        _mayOpen = state;
+    }
 
+    private void OpenPuzzle()
+    {
+        ShowOpenText(false);
+        _isInPuzzle = true;
+        if (_puzzle)
+        {
+            _puzzle.SetActive(true);
+            return;
+        }
+        string puzzleName = $"Prefabs/Puzzles/Puzzle{_puzzleNames[Random.Range(0, 2)]}";
+        _playerController.enabled = false;
+        _puzzle = Instantiate<GameObject>(Resources.Load<GameObject>(puzzleName));
+        _puzzle.transform.SetParent(gameObject.transform);
+        _puzzleButton = _puzzle.transform.Find("Canvas/CompleteButton").GetComponent<Button>();
+        _puzzleButton.onClick.AddListener(OpenSafe);
+    }
 
+    private void ClosePuzzle()
+    {
+        ShowOpenText(true);
+        _isInPuzzle = false;
+        _puzzle.SetActive(false);
+        _playerController.enabled = true;
+    }
+
+    public void OpenSafe()
+    {
+        ClosePuzzle();
+        _puzzleButton.onClick.RemoveAllListeners();
+        GetComponent<SpriteRenderer>().color = Color.white; // —имул€ци€ открыти€ сейфа
+        Destroy(_openText);
+        Destroy(_puzzle);
+        Destroy(this);
+    }
 
 }
