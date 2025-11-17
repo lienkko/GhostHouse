c568c890dde305e5fd0bbfa21e4aef3a660b997e
Artem Lienko <tema.nesmeyanow@yandex.ru>
2025-11-17 15:48:50 +0300

    refactor: переместил код для призрака в отдельный скрипт
    Теперь Game Manager не отвечает за управление призраком
    За логику отвечает WraithHandler
    За столкновение с игроком отвечает WraithCollide

--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    //WraithHandler
-    [SerializeField] private GameObject _objWraith;
-    [SerializeField] private GameObject _textWarning;
-
-    [SerializeField] private Transform _wraithStartPoint;
-    [SerializeField] private Transform _wraithEndPoint;
-
-    private bool _isWraithWaiting = false;
-    //-------------
-
     //PlayerHandler
     [SerializeField] private GameObject _gameOverText;
 
@@ -25,39 +15,10 @@ public class GameManager : MonoBehaviour
         PlayerController.OnDeath += OnPlayerDeath;
     }
 
-
-    private void Update()
-    {
-        if (!_isWraithWaiting)
-            StartCoroutine(WaitForWraith());
-    }
-
-    IEnumerator WaitForWraith()
-    {
-        _isWraithWaiting = true;
-        yield return new WaitForSeconds(5);
-        StartCoroutine(WraithFlight());
-    }
-
-    IEnumerator WraithFlight()
-    {
-        _objWraith.GetComponent<Wraith>().OnFinish += EndWraithFlight;
-
-        _textWarning.SetActive(true);
-        yield return new WaitForSeconds(5);
-        _textWarning.SetActive(false);
-        
-        _objWraith.SetActive(true);
-        _objWraith.GetComponent<Wraith>().StartMoving(_wraithStartPoint.position, _wraithEndPoint.position);
-    }
-
-    private void EndWraithFlight()
-    {
-        _isWraithWaiting = false;
-    }
-
     private void OnPlayerDeath()
     {
         _gameOverText.SetActive(true);
     }
+
+    
 }
