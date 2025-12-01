91ebe405425ddb4d7176530e4a5b9bf8a537c4cc
Artem Lienko <tema.nesmeyanow@yandex.ru>
2025-12-02 01:10:06 +0300

    feat: меню паузы 1 этап

--- Game/Assets/Scripts/UI/Pause.cs
+++ Game/Assets/Scripts/UI/Pause.cs
@@ -1,96 +1,33 @@
-using System.Collections;
-
-using System.Collections.Generic;
-
 using UnityEngine;
-
 using UnityEngine.SceneManagement;
 
-public class pause : MonoBehaviour
-
+public class Pause : MonoBehaviour
 {
-
     public bool paused;
-
     public GameObject pauseMenu;
 
-    void Start()
-
-    {
-
-
-    }
-
-
-    
-
     void Update()
-
     {
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-
-        {
-
-            if (paused)
-
-            {
-
-                Resume();
-
-            }
-
-            else
-
-            {
-
-                Pause();
-
-            }
-
-        }
-
+        
     }
-
-
 
     public void Resume()
-
     {
-
         paused = false;
-
         pauseMenu.SetActive(false);
-
         Time.timeScale = 1f;
-
-
     }
 
-
-
-    public void Pause()
-
+    public void PauseGame()
     {
-
         paused = true;
-
         pauseMenu.SetActive(true);
-
         Time.timeScale = 0f;
-
     }
-
-
 
     public void toMenu()
-
     {
-
         Time.timeScale = 1f;
-
         SceneManager.LoadScene("main menu");
-
     }
-
 }
